41f47d224619052630f959d11397aa0ed86e9194
Clayton Kramp <ckramp@mymail.mines.edu>
2018-04-10 13:19:41 -0600

    Added axis labels

--- main.m
+++ main.m
@@ -2,8 +2,22 @@
 Z = dlmread('bezierPoints.dat');
 X = linspace(0, 100, 100);
 Y = linspace(100, 0, 100);
+figure
+hold on
+title('3D Surface of the Seafloor')
+xlabel('X')
+ylabel('Y')
+zlabel('H')
 surf(X, Y, Z);
+hold off
 
 X = linspace(0, 100, 101);
 Y = dlmread('bezCurve.dat');
-%plot(X, Y);
+figure
+hold on
+title('Bezier Curve of the Seafloor from Epicenter to Power Plant')
+xlabel('X');
+ylabel('H');
+plot(X, Y);
+hold off
+
