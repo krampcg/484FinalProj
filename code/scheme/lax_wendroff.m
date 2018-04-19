7bc03f003a16b17e022fd32687fff19796b7fc6e
John Luke Lusty <jllusty@gmail.com>
2018-04-19 13:51:29 -0600

    Updated Lax-Wendroff
    Now includes the general multi-step method.

--- code/scheme/lax_wendroff.m
+++ code/scheme/lax_wendroff.m
@@ -1,4 +1,4 @@
-% 1D FTCS Scheme
+    % 1D FTCS Scheme
 % Solves 1D Advection Equation
 function u = lax_wendroff(dt,N_t,dx,N_x,u0,c,a,b,f)
 
@@ -17,10 +17,13 @@ r = dt/dx;
 for i=1:N_t-1
     % loop over space
     for j=2:N_x-1
-        % Half Steps
+        % Half Steps w/ Lax-Friedrichs
         u_h_p = 0.5*(u(i,j) + u(i,j+1)) - r/2*c*(f(u(i,j+1))-f(u(i,j)));
         u_h_m = 0.5*(u(i,j) + u(i,j-1)) + r/2*c*(f(u(i,j-1))-f(u(i,j)));
+        % Evaluation of Flux at Half-Steps
+        f_h_p = f(u_h_p);
+        f_h_m = f(u_h_m);
         % Full Step
-        u(i+1,j) = u(i,j) - r*c*(f(u_h_p)-f(u_h_m));
+        u(i+1,j) = u(i,j) - r*c*(f_h_p-f_h_m);
     end
 end
