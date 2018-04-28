52218b5deda5eca1a66c89f90afd0a4cbb9353ca
jonahkopp <38135716+jonahkopp@users.noreply.github.com>
2018-04-28 14:54:46 -0600

    made more correct

--- code/scheme/lax_wendroff.m
+++ code/scheme/lax_wendroff.m
@@ -1,29 +1,60 @@
     % 1D FTCS Scheme
 % Solves 1D Advection Equation
-function u = lax_wendroff(dt,N_t,dx,N_x,u0,c,a,b,f)
+function [u,u_p] = lax_wendroff(dt,N_t,dx,N_x,u0,c,a,b,f,u0_p)
 
 % Solution Discretization
-u = zeros(N_t,N_x);
+% Republican Variables
+u = zeros(N_t,N_x,2);
 % Initial Condition
-u(1,:) = u0;
+u(1,:,:) = u0';
+% Plebian Variables
+u_p = zeros(N_t,N_x);
+% Initial Condition
+u_p(1,:) = u0_p;
 
-% BC's
-u(:,1) = a;
-u(:,N_x) = b;
+r = c*dt/dx;
 
-r = dt/dx;
+Q = (1-0.5*r)/(1+0.5*r);
 
 % loop over time
-for i=1:N_t-1
+for i=1:N_t-1 
+    
+    % Fun
+%     X = floor(rand(50)*N_x);
+%     for p=1:length(X)
+%         u(i,p,2) = u(i,p,1) + 0.1;
+%     end
+    
     % loop over space
     for j=2:N_x-1
         % Half Steps w/ Lax-Friedrichs
-        u_h_p = 0.5*(u(i,j) + u(i,j+1)) - r/2*c*(f(u(i,j+1))-f(u(i,j)));
-        u_h_m = 0.5*(u(i,j) + u(i,j-1)) + r/2*c*(f(u(i,j-1))-f(u(i,j)));
+        % u_h_p = 0.5*(u(i,j,:) + u(i,j+1,:)) - r/2*(f(u(i,j+1,:),j+1)-f(u(i,j,:),j));
+        % u_h_m = 0.5*(u(i,j,:) + u(i,j-1,:)) + r/2*(f(u(i,j-1,:),j-1)-f(u(i,j,:),j));
         % Evaluation of Flux at Half-Steps
-        f_h_p = f(u_h_p);
-        f_h_m = f(u_h_m);
+        % f_h_p = f(u_h_p,j);
+        % 5f_h_m = f(u_h_m,j);
         % Full Step
-        u(i+1,j) = u(i,j) - r*c*(f_h_p-f_h_m);
+        % Alpha Step
+        u(i+1,j,1) = u(i,j,1) + r/2*...
+            (0.5*(u(i,j+1,2)-u(i,j-1,2))+...
+            r*0.25*(u(i,j+1,1)-2*u(i,j,1)+u(i,j-1,1)));
+        % Gamma Step
+        u(i+1,j,2) = u(i,j,2) + r/2*...
+            (0.5*(u(i,j+1,1)-u(i,j-1,1))+...
+            r*0.25*(u(i,j+1,2)-2*u(i,j,2)+u(i,j-1,2)));
+        % Plebian Variable
+        u_p(i+1,j) = u_p(i,j) + dt*u(i,j,2);
+        % u(i+1,j,:) = squeeze(u(i,j,:)) - r*(f_h_p-f_h_m);
     end
+    
+    % Ingoing Sommerfield BC
+    u(i+1,1,1) = u(i,2,1) - u(i+1,2,1)*Q + u(i,1,1)*Q;
+    u(i+1,1,2) = u(i,2,2) - u(i+1,2,2)*Q + u(i,1,2)*Q;
+    % Outgoing Sommerfield BC
+    u(i+1,N_x,1) = u(i,N_x-1,1) - u(i+1,N_x-1,1)*Q + u(i,N_x,1)*Q;
+    u(i+1,N_x,2) = u(i,N_x-1,2) - u(i+1,N_x-1,2)*Q + u(i,N_x,2)*Q;
+
+    % Update Plebian Variable
+    u_p(i+1,1) = u_p(i,1) + dt*u(i,1,2);
+    u_p(i+1,N_x) = u_p(i,N_x) + dt*u(i,N_x,2);
 end
